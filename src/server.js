0f84c6abc07e6e14818f233bf5a5ac642450712c
chayannygois18@gmail.com <you@example.com>
2023-04-24 15:56:20 -0300

    Adicionando

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require ('dotenv').config({path: 'variaveis.env'});
+const express = require ('express');
+const cors = require ('cors');
+const bodyParser = require ('body-parser');
+
+const routes = require ('../src/routes');
+
+const server = express ();
+server.use (
+    '/api'
+    , routes);
+
+server.use(cors ());
+server.use(bodyParser.urlencoded({extend:false}));
+server.listen(process.env.PORT,()=>{
+    console.log('Servidor rodando em http://localhost:${process.env.PORT}')
+}
+)
